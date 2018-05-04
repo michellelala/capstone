9961a73ff837945a6f840f749aa342fdb08eaea9
Michelle <michelleshang@ac.c4q.nyc>
2018-05-04 12:51:02 -0400

    change database connection string

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const pgp = require('pg-promise')({})
-const db = pgp('postgres://localhost/feathers') 
+const db = pgp(process.env.DATABASE_URL) 
 
 module.exports = () => {
     passport.serializeUser((user, done) => {
